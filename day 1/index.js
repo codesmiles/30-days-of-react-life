8653f473b339f48c2066596e77d87a747ddb6bb6
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-12-07 00:31:19 +0100

    CODEWAR CRUISE

--- day 1/index.js
+++ day 1/index.js
@@ -107,6 +107,8 @@ const everyPriceValue = () => {
 };
 console.log(everyPriceValue());
 
+
+
 // CODEWAR CRUISE LOL
 
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
@@ -171,4 +173,17 @@ function basicOp(operation, value1, value2) {
       return false;
     }
   }
+}
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+function squareSum(numbers){
+  sum =0
+  
+   numbers = numbers.map(number=>number*number)
+  for(i=0;i<numbers.length; i++){
+    sum+=numbers[i]
+  }
+  return sum;
 }
